9a6823f1b176e7161ff3c85f496118deb9dc310b
Abdul Vajid <vvajidz56@gmail.com>
2025-12-05 15:07:05 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Navbar } from "@/components/layout/Navbar";
-import { Footer } from "@/components/layout/Footer";
+
 
 
 export const metadata: Metadata = {
@@ -21,7 +21,7 @@ export default function RootLayout({
         <main className="min-h-screen pt-16">
           {children}
         </main>
-        <Footer />
+
       </body>
     </html>
   );
